b0e363ab7e8c83cd3f47c3d64db8a07833719967
Michel A <lifehackr@icloud.com>
2025-03-27 22:32:19 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx"
 import preact from "@astrojs/preact"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
-import { defineConfig, envField } from "astro/config"
+import { defineConfig } from "astro/config"
 import rehypeExternalLinks from "rehype-external-links"
 
 // https://astro.build/config
@@ -18,12 +18,6 @@ export default defineConfig({
   markdown: {
     shikiConfig: { theme: "css-variables", wrap: true },
     rehypePlugins: [[rehypeExternalLinks, { target: "_blank" }]],
-  },
-  env: {
-    schema: {
-      OPENPANEL_DOMAIN: envField.string({ context: "client", access: "public" }),
-      OPENPANEL_URL: envField.string({ context: "client", access: "public" }),
-    },
   },
   site: process.env.SITE_URL || "https://awfys.com",
   integrations: [tailwind({ applyBaseStyles: false }), sitemap(), mdx(), preact()],
